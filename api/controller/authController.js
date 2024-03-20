2bf57154a83a2443e0b979a707515db23cb73987
Bamidele <devbammy@gmail.com>
2024-03-20 14:17:59 +0100

    password

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -6,8 +6,9 @@ import { errorHandler } from '../utils/errorHandler.js';
 // Create a user account
 export const signup = async (req, res, next) => {
   const { fullname, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ fullname, email, password: hashedPassword });
+  // const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ fullname, email, password });
+  // const newUser = new User({ fullname, email, password: hashedPassword });
 
   try {
     await newUser.save();
@@ -26,7 +27,8 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(401, 'Invalid credentials'));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = validUser.password;
+    // const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Invalid credentials'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
