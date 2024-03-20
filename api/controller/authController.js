6a277c56c06130c0b49684af9b37740b6c022040
Bamidele <devbammy@gmail.com>
2024-03-20 14:36:44 +0100

    password

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -6,13 +6,12 @@ import { errorHandler } from '../utils/errorHandler.js';
 // Create a user account
 export const signup = async (req, res, next) => {
   const { fullname, email, password } = req.body;
-  // const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ fullname, email, password });
-  // const newUser = new User({ fullname, email, password: hashedPassword });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ fullname, email, password: hashedPassword });
 
   try {
     await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json('User created successfully');
   } catch (error) {
     if (error.code === 11000) {
       return res.status(400).json({ error: 'Email address is already in use' });
@@ -27,8 +26,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(401, 'Invalid credentials'));
-    const validPassword = validUser.password;
-    // const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Invalid credentials'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
